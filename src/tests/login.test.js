6e2c59a1485b78d0c49603c239bedc2d7f947397
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-04-22 14:41:52 -0300

    tests: login page tests finished

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -8,32 +8,61 @@ import { renderWithRouterAndStore } from './testConfig';
 const VALID_EMAIL = 'email@test.com';
 const INVALID_EMAIL = 'email';
 const VALID_NAME = 'Player One';
-const INVALID_NAME = '';
 
 describe('Login Page Tests', () => {
-  it('Should render Login Page when access the root path (/)', () => {
+  const getFormElements = () => ({
+    emailInput: screen.getByPlaceholderText('Email do Gravatar'),
+    nameInput: screen.getByPlaceholderText('Nome do Jogador'),
+    playButton: screen.getByRole('button', { name: 'Jogar' }),
+    configurationButton: screen.getByRole('button', { name: 'Configurações' }),
+  });
+
+  it('Should render Login Page when access the root path (/).', () => {
     renderWithRouterAndStore(<App />);
 
     const logoImage = screen.getByAltText('Trivia logo');
     expect(logoImage).toBeInTheDocument();
   });
 
-  it('Should have a form to user inset e-mail and name', () => {
+  it('Should have a form to user inset e-mail and name.', () => {
     renderWithRouterAndStore(<App />);
 
-    const emailInput = screen.getByPlaceholderText('Email do Gravatar');
-    const nameInput = screen.getByPlaceholderText('Nome do Jogador');
+    const { emailInput, nameInput } = getFormElements();
     expect(emailInput).toBeInTheDocument();
     expect(nameInput).toBeInTheDocument();
   });
 
+  it('Should have a button that starts the game, changing the url path.', async () => {
+    const { history } = renderWithRouterAndStore(<App />);
+
+    const { emailInput, nameInput, playButton } = getFormElements();
+
+    userEvent.type(emailInput, VALID_EMAIL);
+    userEvent.type(nameInput, VALID_NAME);
+    userEvent.click(playButton);
+
+    const playerName = await screen.findByRole('heading', { level: 1 });
+    expect(playerName).toHaveTextContent(VALID_NAME);
+    expect(history.location.pathname).toBe('/trivia');
+  });
+
+  it('Should have a button that redirect to configuration page.', () => {
+    const { history } = renderWithRouterAndStore(<App />);
+
+    const { configurationButton } = getFormElements();
+
+    userEvent.click(configurationButton);
+
+    const configurationHeader = screen.getByRole('heading', { level: 1 });
+    expect(configurationHeader).toHaveTextContent('Configurações');
+    expect(history.location.pathname).toBe('/configuration');
+  });
+
   it('Should disable the button that starts the game if the '
-    + 'inputs are uncorrectly fulfilled', () => {
+    + 'inputs are uncorrectly fulfilled.', () => {
     renderWithRouterAndStore(<App />);
 
-    const emailInput = screen.getByPlaceholderText('Email do Gravatar');
-    const nameInput = screen.getByPlaceholderText('Nome do Jogador');
-    const playButton = screen.getByRole('button', { name: 'Jogar' });
+    const { emailInput, nameInput, playButton } = getFormElements();
 
     expect(playButton).toHaveAttribute('disabled');
 
@@ -42,7 +71,7 @@ describe('Login Page Tests', () => {
     expect(playButton).toHaveAttribute('disabled');
 
     userEvent.type(emailInput, VALID_EMAIL);
-    userEvent.type(nameInput, INVALID_NAME);
+    userEvent.clear(nameInput);
     expect(playButton).toHaveAttribute('disabled');
 
     userEvent.type(emailInput, VALID_EMAIL);
