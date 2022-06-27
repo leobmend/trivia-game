1efaf56efb56b13b616708d30646445d09fe74e3
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-06-27 16:17:33 -0300

    feat: add express hello world to test docker on heroku functionality

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3001, () => {
+app.get('/', (req, res) => res.send('<h1> Hello world! </h1>'));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
   console.log('Escutando na 3001')
-})
+});
