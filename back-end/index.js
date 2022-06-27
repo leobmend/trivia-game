dedac51b5037d9d26e22690bff3506b13e805cec
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-06-27 16:37:19 -0300

    feat: add port information to the response

--- back-end/index.js
+++ back-end/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1> Hello world!! </h1>'));
-
 const port = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => res.send(`<h1> Hello world!! </h1> <h2> Running on port ${port}. </h2>`));
 
 app.listen(port, () => {
   console.log('Escutando na 3001')
