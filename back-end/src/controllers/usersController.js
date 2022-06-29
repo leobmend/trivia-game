d7cc61070415eeb5e7e742cd5d6767eda7d60320
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-06-29 10:23:53 -0300

    back-feat: add update and remove methods to usersController

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -18,9 +18,31 @@ const signUp = express(async (req, res, _next) => {
   res.status(201).json({ token: jwtToken });
 });
 
+const update = express(async (req, res, _next) => {
+  const { id } = req.params;
+  const { name, email, gravatarUrl } = req.body;
+  
+  const updatedUser = await usersService.update(
+    parseInt(id, 10),
+    { name, email, gravatarUrl },
+  );
+
+  res.status(200).json(updatedUser);
+});
+
+const remove = express(async (req, res, _next) => {
+  const { id } = req.params;
+  
+  await usersService.remove(parseInt(id, 10));
+
+  res.status(200).end();
+});
+
 const usersController = {
   login,
   signUp,
+  update,
+  remove,
 };
 
 module.exports = usersController;
