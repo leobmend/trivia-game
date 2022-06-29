310a6a5441b380bbcbfe4f76be5b550902f4635d
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-06-29 09:10:57 -0300

    back-feat: add signUp method to usersController

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -1,15 +1,26 @@
+const express = require('express-rescue');
+
 const usersService = require('../services/usersService');
 
-const login = async (req, res, _next) => {
+const login = express(async (req, res, _next) => {
   const { email, password } = req.body;
 
   const jwtToken = await usersService.authentication({ email, password });
 
+  res.status(200).json({ token: jwtToken });
+});
+
+const signUp = express(async (req, res, _next) => {
+  const { name, email, password, gravatarUrl } = req.body;
+
+  const jwtToken = await usersService.create({ name, email, password, gravatarUrl });
+
   res.status(201).json({ token: jwtToken });
-};
+});
 
 const usersController = {
   login,
+  signUp,
 };
 
 module.exports = usersController;
