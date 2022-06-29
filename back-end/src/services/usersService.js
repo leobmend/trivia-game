19397fa5e9c6df0bf3c4a7232fc594a80c338e27
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-06-29 10:21:59 -0300

    back-feat: add checkIfUserExists function to userService file

--- back-end/src/services/usersService.js
+++ back-end/src/services/usersService.js
@@ -2,6 +2,12 @@ const { User } = require('../database/models');
 const CustomError = require('../utils/customError');
 const { generateToken } = require('../utils/jwtUtils');
 
+const checkIfUserExists = async (id) => {
+  const oldUser = await User.findByPk(id);
+
+  if (!oldUser) throw new CustomError(404, 'User does not exist');
+};
+
 const authentication = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
 
