9a4f8a65f74fe6f4a4e9bba2561f216aeb195d0e
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-07-02 19:15:49 -0300

    front-feat: add handleEdit func and the logic to edit users on profile page

--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -1,16 +1,42 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import getGravatar from '../../services/gravatar';
 import './style.css';
+
+import getGravatar from '../../services/gravatar';
+import { fetchEditUser } from '../../redux-test/player';
 
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const player = useSelector((state) => state.player.info);
+  const dispatch = useDispatch();
 
   const history = useHistory();
 
   const handleClick = () => {
     history.push('/');
   };
+
+  const handleEdit = () => {
+    const newName = name || player.name;
+    const newEmail = email || player.email;
+    dispatch(fetchEditUser(
+      { id: player.id, userToken: player.userToken, name: newName, email: newEmail },
+    ));
+  };
+
+  const renderInput = (current, state, setFunc) => (
+    <input
+      type="text"
+      className="edit-input"
+      value={ state }
+      placeholder={ current }
+      onChange={ ({ target }) => setFunc(target.value) }
+    />
+  );
 
   return (
     <main className="Profile">
@@ -48,25 +74,28 @@ const Profile = () => {
           <h2>Name</h2>
           {
             isEditing
-              ? <input type="text" className="edit-input" />
-              : <p>Jogador</p>
+              ? renderInput(player.name, name, setName)
+              : <p>{ player.name }</p>
           }
         </div>
         <div className="info-container">
           <h2>E-mail</h2>
           {
             isEditing
-              ? <input type="text" className="edit-input" />
-              : <p>jogador@email.com</p>
+              ? renderInput(player.email, email, setEmail)
+              : <p>{ player.email }</p>
           }
         </div>
         <button
-          className="home-button"
+          className={ `home-button ${isEditing && 'editing'}` }
           data-testid="btn-go-home"
           type="button"
-          onClick={ () => setIsEditing(!isEditing) }
+          onClick={ () => {
+            if (isEditing) handleEdit();
+            else setIsEditing(!isEditing);
+          } }
         >
-          Edit name / e-mail
+          {`${isEditing ? 'Confirm ' : ''}Edit name / e-mail`}
         </button>
         <button
           className="home-button"
