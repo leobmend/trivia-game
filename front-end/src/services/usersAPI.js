4550e86368808af7caa9c5ccc549c8e0705f62f9
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-07-02 19:13:44 -0300

    front-feat: add update method to usersAPI service

--- front-end/src/services/usersAPI.js
+++ front-end/src/services/usersAPI.js
@@ -7,18 +7,32 @@ const api = axios.create({
 const login = async ({ email, password }) => (
   api.post('/login', { email, password })
     .then((response) => response.data)
-    .catch((err) => console.log(err.message))
+    .catch((err) => err.message)
 );
 
 const signUp = async ({ name, email, password, gravatarUrl }) => (
   api.post('/user/signup', { name, email, password, gravatarUrl })
     .then((response) => response.data)
-    .catch((err) => console.log(err.message))
+    .catch((err) => err.message)
+);
+
+const getById = async (id, userToken) => (
+  api.get(`/user/${id}`, { headers: { Authorization: userToken } })
+    .then((response) => response.data)
+    .catch((err) => err.message)
+);
+
+const update = async (id, userToken, { name, email }) => (
+  api.put(`/user/${id}`, { name, email }, { headers: { Authorization: userToken } })
+    .then((response) => response.data)
+    .catch((err) => err.message)
 );
 
 const usersAPI = {
   login,
   signUp,
+  getById,
+  update,
 };
 
 export default usersAPI;
