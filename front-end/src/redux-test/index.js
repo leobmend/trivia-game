88242e817018651a6c55e3ff7509fc0415c390a3
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-07-04 12:22:07 -0300

    front-feat: add loading redux slice store

--- front-end/src/redux-test/index.js
+++ front-end/src/redux-test/index.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import playerReducer from './player';
 import scoreReducer from './score';
 import triviaReducer from './trivia';
+import loadingReducer from './loading';
 
 export default configureStore({
   reducer: {
     player: playerReducer,
     score: scoreReducer,
     trivia: triviaReducer,
+    loading: loadingReducer,
   },
 });
