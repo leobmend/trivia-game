021ccf3ef193af9f7332b79a07c5dcaeaaae0b05
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-07-08 18:18:35 -0300

    front-refactor: change feedback to english

--- front-end/src/pages/Feedback/index.js
+++ front-end/src/pages/Feedback/index.js
@@ -11,11 +11,52 @@ const Feedback = () => {
 
   const history = useHistory();
 
-  const feedback = () => {
+  const renderFeedback = () => {
     if (assertions < THREE_ASSERTIONS) {
       return (<h1 className="fb-h1" data-testid="feedback-text">Could be better...</h1>);
     }
     return (<h1 className="fb-h1" data-testid="feedback-text">Well Done!</h1>);
+  };
+
+  const renderAssertionsAndScore = () => {
+    switch (assertions) {
+    case 0:
+      return (
+        <h2 className="fb-h2">
+          You didn&apos;t get any questions right!
+        </h2>
+      );
+    case 1:
+      return (
+        <>
+          <h2 className="fb-h2">
+            You got
+            <span data-testid="feedback-total-question"> 1 </span>
+            question right!
+          </h2>
+          <h2 className="fb-h2">
+            And have made
+            <span data-testid="feedback-total-score">{` ${score} `}</span>
+            points.
+          </h2>
+        </>
+      );
+    default:
+      return (
+        <>
+          <h2 className="fb-h2">
+            You got
+            <span data-testid="feedback-total-question">{` ${assertions} `}</span>
+            questions right!
+          </h2>
+          <h2 className="fb-h2">
+            And have made
+            <span data-testid="feedback-total-score">{` ${score} `}</span>
+            points.
+          </h2>
+        </>
+      );
+    }
   };
 
   return (
@@ -23,17 +64,8 @@ const Feedback = () => {
       <section className="feedback-container">
         <div className="feedback-box">
           <div className="feedback-text-container">
-            {feedback()}
-            <h2 className="fb-h2">
-              Voce acertou
-              <span data-testid="feedback-total-question">{` ${assertions} `}</span>
-              questões!
-            </h2>
-            <h2 className="fb-h2">
-              Um total de
-              <span data-testid="feedback-total-score">{` ${score} `}</span>
-              pontos.
-            </h2>
+            {renderFeedback()}
+            {renderAssertionsAndScore()}
           </div>
           <div className="fb-buttons-container">
             <button
